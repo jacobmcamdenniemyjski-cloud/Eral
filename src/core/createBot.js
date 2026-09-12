7b16087aa73e69d7ddb770e8bc061b7c25e3cd2b
Jacob Camdenniemyjski <jacob.m.camdenniemyjski@gmail.com>
2026-09-13 05:40:31 +0900

    Add Mineflayer Pathfinder

--- src/core/createBot.js
+++ src/core/createBot.js
@@ -1,13 +1,18 @@
 const mineflayer = require('mineflayer')
+const { pathfinder } = require('mineflayer-pathfinder')
 
 function createBot() {
-  return mineflayer.createBot({
+  const bot = mineflayer.createBot({
     host: 'localhost',
     port: 25565,
     username: 'earl',
     auth: 'offline',
     version: '1.21.11'
   })
+
+  bot.loadPlugin(pathfinder)
+
+  return bot
 }
 
 module.exports = createBot
